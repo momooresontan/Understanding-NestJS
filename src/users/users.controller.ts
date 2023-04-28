0b52e888a0c25f1430145eff9e4bb8d355afc0d4
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-04-28 02:32:48 +0100

    user entity created

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,25 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from '/dto/create-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
-  getUsers(): any {
+  getUsers(): User[] {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  getUserById(@Param('id') id: string): any {
+  getUserById(@Param('id') id: string): User {
     //TODO: auto parse ID
     return this.usersService.findById(Number(id));
   }
 
   @Post()
-  createUser(@Body() body: CreateUserDto): any {
-    return this.usersService.createUser();
+  createUser(@Body() body: CreateUserDto): User {
+    return this.usersService.createUser(body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private users: any = [{ id: 0, name: 'MoMo' }];
+  private users: User[] = [{ id: 0, name: 'MoMo' }];
 
-  findAll() {
+  findAll(): User[] {
     return this.users;
   }
 
-  findById(userId: number) {
+  findById(userId: number): User {
     return this.users.find((user) => user.id === userId);
   }
 
-  createUser(name: string) {
-    const newUser = { id: Date.now(), name };
+  createUser(createUserDto: CreateUserDto): User {
+    const newUser = { id: Date.now(), ...createUserDto };
 
     this.users.push(newUser);
 
