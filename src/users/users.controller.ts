baa604175be1b5d6bc40da93e0612798722b2509
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-04-28 02:14:33 +0100

    user dto created

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from '/dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -14,5 +15,10 @@ export class UsersController {
   getUserById(@Param('id') id: string): any {
     //TODO: auto parse ID
     return this.usersService.findById(Number(id));
+  }
+
+  @Post()
+  createUser(@Body() body: CreateUserDto): any {
+    return this.usersService.createUser();
   }
 }
